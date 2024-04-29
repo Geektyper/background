453dd774908ba2461e273db13d5267240046e866
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-04-29 13:37:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
 class Config:
     TOKEN = "your_bot_token_here"
-    RMBG_API_KEY = "your_remove_bg_api_key_here"
-class Config:
-    TOKEN = "your_bot_token_here"
-    RMBG_API_KEY = "your_remove_bg_api_key_here"
+    RMBG = "your_remove_bg_api_key_here"
